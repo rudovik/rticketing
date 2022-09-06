7fd90042bad0580eabef8b9439ed2a80cf7dad2c
rudovik <flashwrite@ukr.net>
2022-09-06 17:14:47 +0300

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,6 +9,7 @@ const closeMongooseConnection = () => {
 }
 
 const start = async () => {
+  console.log("Starting up...")
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined")
   }
